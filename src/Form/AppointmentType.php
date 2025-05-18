0edcab83d0cb7333b39acbd26175db1a6aa1bcd5
ilianj21 <ilianjean6264@gmail.com>
2025-05-18 13:16:11 +0200

    maj

--- src/Form/AppointmentType.php
+++ src/Form/AppointmentType.php
@@ -1,10 +1,12 @@
 <?php
 
+// src/Form/AppointmentType.php
 namespace App\Form;
 
 use App\Entity\Appointment;
 use App\Entity\TimeSlot;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,19 +17,37 @@ class AppointmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('status')
             ->add('patient', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'placeholder' => 'Sélectionnez un patient',
+                'query_builder' => function(UserRepository $repo) {
+                    return $repo->createQueryBuilder('u')
+                        ->andWhere('u.roles LIKE :role')
+                        ->setParameter('role', '%ROLE_USER%')
+                        ->orderBy('u.name', 'ASC');
+                },
             ])
             ->add('doctor', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'placeholder' => 'Sélectionnez un médecin',
+                'query_builder' => function(UserRepository $repo) {
+                    return $repo->createQueryBuilder('u')
+                        ->andWhere('u.roles LIKE :role')
+                        ->setParameter('role', '%ROLE_DOCTOR%')
+                        ->orderBy('u.name', 'ASC');
+                },
             ])
             ->add('timeSlot', EntityType::class, [
                 'class' => TimeSlot::class,
-                'choice_label' => 'id',
+                'choice_label' => function(TimeSlot $ts) {
+                    return $ts->getStartAt()->format('d/m/Y H:i')
+                         . ' – ' . $ts->getEndAt()->format('H:i');
+                },
+                'placeholder' => 'Sélectionnez un créneau',
             ])
+            ->add('status')
         ;
     }
 
@@ -38,3 +58,4 @@ class AppointmentType extends AbstractType
         ]);
     }
 }
+
--- src/Form/TimeSlotType.php
+++ src/Form/TimeSlotType.php
@@ -1,9 +1,10 @@
 <?php
-
+// src/Form/TimeSlotType.php
 namespace App\Form;
 
 use App\Entity\TimeSlot;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,15 +15,18 @@ class TimeSlotType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('endAt', null, [
-                'widget' => 'single_text',
-            ])
+            ->add('startAt')
+            ->add('endAt')
             ->add('doctor', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'placeholder' => 'Sélectionnez un médecin',
+                'query_builder' => function(UserRepository $repo) {
+                    return $repo->createQueryBuilder('u')
+                        ->andWhere('u.roles LIKE :role')
+                        ->setParameter('role', '%ROLE_DOCTOR%')
+                        ->orderBy('u.name', 'ASC');
+                },
             ])
         ;
     }
