2c12a8c33a21c6e8f82fa349f29f8b5b4c88cd5e
ilianj21 <ilianjean6264@gmail.com>
2025-05-20 20:24:26 +0200

    maj 3

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,10 +1,8 @@
 <?php
 // src/Controller/SecurityController.php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,18 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'login')]
-    public function login(AuthenticationUtils $helper): Response
+    public function login(AuthenticationUtils $authUtils): Response
     {
+        // s’il y a déjà un utilisateur connecté, on le redirige
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_appointment_index');
+        }
+
         return $this->render('security/login.html.twig', [
-            'error'         => $helper->getLastAuthenticationError(),
-            'last_username' => $helper->getLastUsername(),
+            'last_username' => $authUtils->getLastUsername(),
+            'error'         => $authUtils->getLastAuthenticationError(),
         ]);
     }
 
     #[Route(path: '/logout', name: 'logout')]
     public function logout(): void
     {
-        // Symfony gère la déconnexion automatiquement, jamais atteint.
+        // Symfony gère la déconnexion, pas besoin de code ici
         throw new \LogicException('This method can be blank – it will be intercepted by the logout key on your firewall.');
     }
 }
